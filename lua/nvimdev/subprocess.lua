4c261a74ec44dc7ca62b0ceb5106f752208b24b3
Lewis Russell <lewis6991@gmail.com>
2022-06-22 17:21:25 +0100

    feat: add uncrustify diagnostics

--- lua/nvimdev/subprocess.lua
+++ lua/nvimdev/subprocess.lua
@@ -3,8 +3,14 @@ local uv = vim.loop
 local M = {}
 
 function M.subprocess(opts, on_exit)
-  local stdout = uv.new_pipe(false)
-  local stderr = uv.new_pipe(false)
+  if opts.stdout == nil then
+    opts.stdout = uv.new_pipe(false)
+  end
+
+  if opts.stderr == nil then
+    opts.stderr = uv.new_pipe(false)
+  end
+
   local stdin
 
   if opts.input then
@@ -17,39 +23,43 @@ function M.subprocess(opts, on_exit)
   local handle, err = uv.spawn(opts.command, {
     args = opts.args,
     cwd = opts.cwd,
-    stdio = { stdin, stdout, stderr },
+    stdio = { stdin, opts.stdout or nil, opts.stderr  or nil},
   },
     function(code)
-      if stdout then stdout:read_stop() end
-      if stderr then stderr:read_stop() end
+      if opts.stdout then opts.stdout:read_stop() end
+      if opts.stderr then opts.stderr:read_stop() end
 
-      if stdout and not stdout:is_closing() then stdout:close() end
-      if stderr and not stderr:is_closing() then stderr:close() end
-      if stdin  and not stdin:is_closing()  then stdin:close() end
+      if opts.stdout and not opts.stdout:is_closing() then opts.stdout:close() end
+      if opts.stderr and not opts.stderr:is_closing() then opts.stderr:close() end
+      if stdin       and not       stdin:is_closing()  then stdin:close() end
 
       on_exit(code, stdout_data, stderr_data)
     end
   )
 
   if not handle then
-    if stdout and not stdout:is_closing() then stdout:close() end
-    if stderr and not stderr:is_closing() then stderr:close() end
+    if opts.stdout and not opts.stdout:is_closing() then opts.stdout:close() end
+    if opts.stderr and not opts.stderr:is_closing() then opts.stderr:close() end
     if stdin  and not stdin:is_closing()  then stdin:close() end
     opts.input = nil
     error('Failed to spawn process '..err..'\n'..vim.inspect(opts))
   end
 
-  stdout:read_start(function(_, data)
-    if data then
-      stdout_data = stdout_data..data
-    end
-  end)
+  if opts.stdout then
+    opts.stdout:read_start(function(_, data)
+      if data then
+        stdout_data = stdout_data..data
+      end
+    end)
+  end
 
-  stderr:read_start(function(_, data)
-    if data then
-      stderr_data = stderr_data..data
-    end
-  end)
+  if opts.stderr then
+    opts.stderr:read_start(function(_, data)
+      if data then
+        stderr_data = stderr_data..data
+      end
+    end)
+  end
 
   if opts.input then
     stdin:write(opts.input)
