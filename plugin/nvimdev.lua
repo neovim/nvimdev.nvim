bcb94b506d147cd04b52b98586697a9e5fc0dda9
Lewis Russell <lewis6991@gmail.com>
2022-11-10 13:41:26 +0000

    remove old workaround

--- plugin/nvimdev.lua
+++ plugin/nvimdev.lua
@@ -2,17 +2,11 @@ local api = vim.api
 
 api.nvim_create_user_command('NvimTestRun', function(props)
   require('nvimdev').run_test(props)
-end, {
-  force = true,
-  nargs = '*' -- shouldn't need this. Must be a bug.
-})
+end, { force = true })
 
 api.nvim_create_user_command('NvimTestClear', function()
   require('nvimdev').clear_test_decor()
-end, {
-  force=true,
-  nargs='*' -- shouldn't need this. Must be a bug.
-})
+end, { force = true })
 
 api.nvim_create_autocmd('BufRead', {
   group = api.nvim_create_augroup('nvimdev_clint', {}),
